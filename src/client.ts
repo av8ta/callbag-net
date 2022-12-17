2bf92c76dafcd4502028c51a811ae53e4c652fa0
av8ta <av8ta@pm.me>
2022-12-17 19:31:17 +0800

    add test to connect and receive data

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,14 @@
 import { SourceInitiator } from 'callbag-types'
 import { SocketConnectOpts } from 'node:net'
+import { pipe, fromIter } from 'callbag-basics-esmodules'
 
 export function Client(options?: SocketConnectOpts): typeof SourceInitiator {
   console.log(options)
- }
+  const message = 'hello\r\n'
+
+  return pipe(
+    fromIter([message])
+  )
+}
+
+
--- test/client.test.js
+++ test/client.test.js
@@ -2,9 +2,9 @@ import { describe } from 'node:test'
 import assert from 'node:assert/strict'
 import { pipe, fromIter } from 'callbag-basics-esmodules'
 import subscribe from 'callbag-subscribe'
+import { Client } from '../lib/client.js'
+import { Signal } from '../lib/typings/types.js'
 import net from 'node:net'
-import Debug from 'debug'
-const log = Debug('callbag-net:client:test')
 
 describe('1 equals 1', () => {
   return new Promise((resolve, reject) => {
@@ -22,32 +22,62 @@ describe('1 equals 1', () => {
 describe('Creates test server', () => {
   return new Promise((resolve, _reject) => {
     const server = createServer('hello')
-    log(server instanceof net.Server)
     resolve(assert(server instanceof net.Server))
     server.close()
   })
 })
 
+describe('Client receives data from server', () => {
+  const message = 'hello\r\n'
+  const server = createServer(message)
+  const source = Client({ port: 8124, host: 'localhost' })
+
+  return new Promise((resolve, reject) => {
+    pipe(
+      source,
+      subscribe({
+        next: v => {
+          if (v.toString() === message) {
+            console.log('Data received from server:', message)
+            source(Signal.END)
+            server.close()
+            resolve('Pass')
+          }
+          else {
+            server.close()
+            reject()
+          }
+        },
+        error: e => {
+          server.close()
+          reject(e)
+        },
+        complete: () => { server.close() }
+      })
+    )
+  })
+})
+
 function createServer(message) {
   const server = net.createServer(client => {
-    log('client connected')
+    console.log('client connected')
     client.on('end', () => {
-      log('client disconnected')
+      console.log('client disconnected')
     })
     client.write(message)
   })
 
   server.on('error', error => {
-    log(error)
+    console.log(error)
     throw error
   })
 
   server.on('close', () => {
-    log('close event')
+    console.log('close event')
   })
 
   server.listen(2222, () => {
-    log('server bound')
+    console.log('server bound')
   })
   return server
 }
